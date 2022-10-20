bf276499fd928877af0a7f57af1b4f3e1b5a109e
John Klassen <klassenjo@hotmail.com>
2022-10-20 05:04:58 -0400

    improve tiered hand logic, update test files

--- packages/game/src/lib/create-game.test.ts
+++ packages/game/src/lib/create-game.test.ts
@@ -1,10 +1,7 @@
 import { describe, expect, it } from 'vitest';
-import { CARDS } from './cards';
 import { createGame } from './create-game';
 
 describe('game', () => {
-  const firstFiveCards = [CARDS[0], CARDS[1], CARDS[2], CARDS[3], CARDS[4]];
-
   it('should initialize an empty board', () => {
     const { board } = createGame();
 
@@ -20,28 +17,5 @@ describe('game', () => {
 
     expect(playerOne.label).toBe('one');
     expect(playerTwo.label).toBe('two');
-  });
-
-  it('should reflect that each player has the same hand', () => {
-    const { playerOne, playerTwo } = createGame();
-
-    expect(playerOne.hand.length).toBe(5);
-    expect(playerTwo.hand.length).toBe(5);
-
-    // TODO: Should we elevate this to a custom predicate and use toSatisfy?
-    for (const card of playerOne.hand) {
-      expect(playerTwo.hand).toContainEqual(card);
-    }
-  });
-
-  it('returns players with hands limited to custom cards', () => {
-    const { playerOne, playerTwo } = createGame({
-      cards: firstFiveCards,
-    });
-
-    // TODO: Should we elevate this to a custom predicate and use toSatisfy?
-    for (const card of playerOne.hand) {
-      expect(playerTwo.hand).toContainEqual(card);
-    }
   });
 });
--- packages/game/src/lib/pull-tiered-hand.test.ts
+++ packages/game/src/lib/pull-tiered-hand.test.ts
@@ -1,12 +1,14 @@
 import { assert, describe, expect, it } from 'vitest';
 import { CARDS } from './cards';
-import { pullTieredHand } from './pull-tiered-hand';
+import { InsufficientLevelRangeError } from './errors';
+import { generateLevelLoadout, pullTieredHand } from './pull-tiered-hand';
 
-describe.only('pullTieredHand', () => {
-  // const firstFiveCards = [CARDS[0], CARDS[1], CARDS[2], CARDS[3], CARDS[4]];
+describe('pullTieredHand', () => {
+  const firstFiveCards = [CARDS[0], CARDS[1], CARDS[2], CARDS[3], CARDS[4]];
 
-  it.only('should create a hand restricted by level limits', () => {
-    const hand = pullTieredHand(CARDS, [3, 3, 6, 8, 10]);
+  it('should create a hand restricted by level limits', () => {
+    const levels = generateLevelLoadout(firstFiveCards, true);
+    const hand = pullTieredHand(firstFiveCards, levels);
 
     assert(hand[0]);
     assert(hand[1]);
@@ -14,10 +16,16 @@ describe.only('pullTieredHand', () => {
     assert(hand[3]);
     assert(hand[4]);
 
-    expect(hand[0].level).toEqual(3);
-    expect(hand[1].level).toEqual(3);
-    expect(hand[2].level).toEqual(6);
-    expect(hand[3].level).toEqual(8);
-    expect(hand[4].level).toEqual(10);
+    expect(hand[0].level).toEqual(1);
+    expect(hand[1].level).toEqual(1);
+    expect(hand[2].level).toEqual(1);
+    expect(hand[3].level).toEqual(1);
+    expect(hand[4].level).toEqual(1);
+  });
+
+  it('should throw InsufficientLevelRangeError when duplicates are disabled and level range is too small', () => {
+    expect(() => generateLevelLoadout(firstFiveCards)).toThrowError(
+      InsufficientLevelRangeError,
+    );
   });
 });
