e881de1a126e1d0c5f1f548ccd457344227cad02
Jake Klassen <jklassendev@gmail.com>
2022-10-20 16:16:17 -0400

    fix while loop issue

--- packages/game/src/lib/pull-tiered-hand.ts
+++ packages/game/src/lib/pull-tiered-hand.ts
@@ -2,20 +2,29 @@ import groupBy from 'just-group-by';
 import randomInteger from 'just-random-integer';
 import shuffle from 'just-shuffle';
 import { Card, Hand } from './common-types';
+import { HAND_SIZE } from './constants';
 import { InsufficientLevelRangeError } from './errors';
 
 const getAvailableLevels = (cards: Card[]) => {
   const availableLevels = Array.from(new Set(cards.map((card) => card.level)));
+
   return availableLevels;
 };
 
+/**
+ * This function will return an array of `HAND_SIZE = 5` card levels that
+ * are available, and optionally support returning duplicates.
+ * @param cards
+ * @param allowDuplicates
+ * @returns
+ */
 export const generateLevelLoadout = (
   cards: Card[],
   allowDuplicates = false,
 ) => {
   const availableLevels = getAvailableLevels(cards);
 
-  if (allowDuplicates === false && availableLevels.length < 5) {
+  if (allowDuplicates === false && availableLevels.length < HAND_SIZE) {
     throw new InsufficientLevelRangeError();
   }
 
@@ -23,11 +32,10 @@ export const generateLevelLoadout = (
   const maximumLevel = availableLevels[availableLevels.length - 1];
   const levels: number[] = [];
 
-  while (levels.length < 5) {
+  while (levels.length < HAND_SIZE) {
     const num = randomInteger(minimumLevel, maximumLevel);
 
-    if (allowDuplicates === false && levels.includes(num) === false) {
-      levels.push(num);
+    if (allowDuplicates === false && levels.includes(num)) {
       continue;
     }
 
@@ -37,6 +45,13 @@ export const generateLevelLoadout = (
   return levels.sort((a, b) => a - b);
 };
 
+/**
+ * Return a hand based on the provided cards and level loadout.
+ * @param cards
+ * @param levelLoadout
+ * @param allowDuplicates
+ * @returns
+ */
 export const pullTieredHand = (
   cards: Card[],
   levelLoadout: number[],
@@ -45,11 +60,10 @@ export const pullTieredHand = (
   const cardsByLevel = groupBy(cards, (c) => c.level);
   const hand: Card[] = [];
 
-  while (hand.length < 5) {
+  while (hand.length < HAND_SIZE) {
     const card = shuffle(cardsByLevel[levelLoadout[hand.length]])[0];
 
-    if (allowDuplicates === false && hand.includes(card) === false) {
-      hand.push(card);
+    if (allowDuplicates === false && hand.includes(card)) {
       continue;
     }
 
