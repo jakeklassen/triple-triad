9fc1a2b91f75e247b44c23acf764acf2a994542e
Jake Klassen <jklassendev@gmail.com>
2022-10-23 07:41:48 -0400

    some notes on supabase status

--- packages/backend-socketio/src/index.ts
+++ packages/backend-socketio/src/index.ts
@@ -1,6 +1,27 @@
-import { Server } from 'colyseus';
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
 const port = parseInt(process.env.PORT ?? '3000', 10);
 
-const gameServer = new Server();
-gameServer.listen(port);
-console.log(`[GameServer] Listening on Port: ${port}`);
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Are you supposed to be here?',
+  });
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`[GameServer] Listening on Port: ${port}`);
+});
