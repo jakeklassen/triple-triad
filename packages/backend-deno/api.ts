a31b61be17cc1731c4f6185f1ac1537557d97eba
Jake Klassen <jklassendev@gmail.com>
2022-10-25 16:22:26 -0400

    package upgrades and some deno file changes

---
+++ packages/backend-deno/api.ts
@@ -0,0 +1,45 @@
+import { serve } from 'https://deno.land/std@0.160.0/http/server.ts';
+
+const sockets = new Set<WebSocket>();
+
+const channel = new BroadcastChannel('chat');
+
+channel.onmessage = (e) => {
+  if (e.target != channel) {
+    channel.postMessage(e.data);
+  }
+
+  for (const socket of sockets) {
+    socket.send(
+      JSON.stringify({
+        payload: e.data,
+        count: sockets.size,
+      }),
+    );
+  }
+};
+
+serve((req) => {
+  const upgrade = req.headers.get('upgrade') ?? '';
+
+  // If this is not a websocket upgrade, return the initial HTML page.
+  if (upgrade.toLowerCase() != 'websocket') {
+    return new Response('Not Found', {
+      status: 404,
+      headers: {
+        'content-type': 'text/plain',
+      },
+    });
+  }
+
+  const { socket, response } = Deno.upgradeWebSocket(req);
+
+  sockets.add(socket);
+
+  socket.onopen = () => console.log('socket opened');
+  socket.onerror = (e) => console.log('socket errored:', e);
+  socket.onmessage = channel.onmessage as WebSocket['onmessage'];
+  socket.onclose = () => sockets.delete(socket);
+
+  return response;
+});
